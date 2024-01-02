61597400ceadc0b24f471f0748446739c7be31b0
luiza <luiza.leal@hsl.org.br>
2024-01-02 15:10:22 -0300

    Update index

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get('/pessoa', async (req, res) => {
     const client = pool.getConnection();
     const result = pool.query('SELECT * FROM pessoa');
     res.json(result.rows);
-    pool.release();
   } catch (err) {
     console.error(err);
     res.send("Erro " + err);
