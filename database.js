8892e4da13e6dd2fd689fcb91cf9e4c74ea9d001
luiza <luiza.leal@hsl.org.br>
2024-01-03 11:07:12 -0300

    Update

--- database.js
+++ database.js
@@ -2,13 +2,13 @@ const mysql = require('mysql2')
  
 const pool = mysql.createPool({
  host: process.env.DB_HOST,
- //port: process.env.DB_PORT,
  user: process.env.DB_USERNAME,
  password: process.env.DB_PASSWORD,
  database: process.env.DB_DBNAME,
  waitForConnections:true,
  connectionLimit:1000,
- queueLimit:0
+ queueLimit:0,
+ port: 3308
 })
 
 pool.getConnection((err,conn) => {
