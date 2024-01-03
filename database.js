714167050ed2cfda4dc6f299b1e5d7bb96083fca
luiza <luiza.leal@hsl.org.br>
2024-01-03 10:46:24 -0300

    Update database

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
  
 const pool = mysql.createPool({
  host: process.env.DB_HOST,
- port: process.env.DB_PORT,
+ //port: process.env.DB_PORT,
  user: process.env.DB_USERNAME,
  password: process.env.DB_PASSWORD,
  database: process.env.DB_DBNAME,
