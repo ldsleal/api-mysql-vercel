e6c1ec956035da4a1de4a742e14c9f64e146a6b7
luiza <luiza.leal@hsl.org.br>
2024-01-08 11:12:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ app.post('/batimentos_cardiacos', async (req, res) => {
           [id_pessoa, valor, dia, horario]
       );
 
-      res.status(201).json(result.rows[0]);
+      res.status(201).json(result[0]);
   } catch (error) {
       console.error('Erro ao inserir no banco de dados', error);
       res.status(500).send('Erro interno do servidor');
